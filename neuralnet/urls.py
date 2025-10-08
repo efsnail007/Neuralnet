0ea2394f5f6f6c8a9ff7351e31aa4f0c121880f3
snail <sgefsnkiospch908@gmail.com>
2025-10-08 20:10:22 +0300

    four_labs

--- neuralnet/urls.py
+++ neuralnet/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("bert_classifier/", include("bert_classifier.urls")),
     path("dialog_bot/", include("dialog_bot.urls")),
+    path("image_classification/", include("image_classification.urls")),
 ]
