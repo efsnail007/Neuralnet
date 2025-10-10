75cb6becf87f548e1df71f68dd51b1f7ebeeb162
snail <sgefsnkiospch908@gmail.com>
2025-10-10 16:44:24 +0300

    end

--- neuralnet/urls.py
+++ neuralnet/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("bert_classifier/", include("bert_classifier.urls")),
     path("dialog_bot/", include("dialog_bot.urls")),
     path("image_classification/", include("image_classification.urls")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
